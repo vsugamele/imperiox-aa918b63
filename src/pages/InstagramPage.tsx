96c6c88010be8c6582ccad594ae07681e95c8e7f
Vinicius <vsugamele@gmail.com>
2026-06-07 00:10:55 -0300

    fix: restore missing Card tag in InstagramPage.tsx

--- src/pages/InstagramPage.tsx
+++ src/pages/InstagramPage.tsx
@@ -2401,6 +2401,7 @@ export default function InstagramPage() {
                           </div>
                         )}
                       </CardContent>
+                    </Card>
                   </div>
                 )}
 
